25f7acb60f0a90413203a35ab9253fe78b52d643
Minhtuannn <ngominhtuan311@gmail.com>
2024-10-05 18:43:47 +0000

    edit url

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728152382',resources: [
+    cacheName: 'chirpy-1728153813',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
